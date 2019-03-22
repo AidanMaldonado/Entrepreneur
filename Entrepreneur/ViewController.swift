7e360a8055f97a3c937df330609ad01cada74bdb
Aidan Maldonado <aidanm1198@gmail.com>
2019-03-22 11:27:44 -0400

    Basic Setup

--- Entrepreneur/ViewController.swift
+++ Entrepreneur/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var openingText = ["You just turned 18!", "Your grandma gave you $15 for your birthday", "Your dream is to become the worlds richest man before you turn 21", "You have 1000 days to make 100 billion dollars", "Good luck!"]
+    
+    //Game Variables
+    var money = 0
+    
+    //Game Labels
+    @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
